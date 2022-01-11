6b752a13fdfaf50ffdd456805212c107415fbc01
Daniel <dkilcullen25@gmail.com>
2022-01-11 17:48:29 -0600

    commit tree 5842d318925504e422e8c0aaef5dd8f83118f60a

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  
+  //initialize state to not in cart
+  const [inCart, setInCart] = useState(false)
+
+  function handleAddToCart() {
+    setInCart( (inCart) => {
+      return !inCart
+    } )
+  }
+
   return (
-    <li className="">
+    <li className={ inCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleAddToCart} className="add">Add to Cart</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  let [selectedCategory, setSelectedCategory] = useState('')
+
+  function handleSetSelectedCategory(e) {
+    setSelectedCategory( (prevCategory) =>  e.target.value )
+  }
+  
+  function renderList() {
+    //show all options if loading in for the first time (i.e. if state is empty string)
+    if(selectedCategory === '') {
+      return items.map((item) => <Item key={item.id} name={item.name} category={item.category} /> )
+    } else {
+      //else just show the options with item.category matching the selection
+      return items.map((item) => item.category === selectedCategory ? <Item key={item.id} name={item.name} category={item.category} /> : '')
+    }
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleSetSelectedCategory}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,9 +29,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {renderList()}
       </ul>
     </div>
   );
